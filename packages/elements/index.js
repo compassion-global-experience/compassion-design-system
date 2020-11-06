46431b1c2f124b6cccfd26bb91f2271772717163
Kendrick Arnett <kmarnett@gmail.com>
2020-11-05 19:11:47 -0600

    feat(elements): Add helpers to Elements: breakpoints, color, motion, space, typography (#9)

---
+++ packages/elements/index.js
@@ -0,0 +1,11 @@
+import b from './src/breakpoints';
+import c from './src/color';
+import m from './src/motion';
+import s from './src/space';
+import * as t from './src/typography';
+
+export const breakpoints = b;
+export const color = c;
+export const motion = m;
+export const space = s;
+export const typography = t;
