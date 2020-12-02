8db9c849a98136f9a90838b7ed0d50120bd5b453
Kendrick Arnett <kmarnett@gmail.com>
2020-12-02 13:11:53 -0600

    feat(react-web): Add and consume ThemeProvider to allow definition of styles outside of components

--- packages/components/react-web/.storybook/preview.js
+++ packages/components/react-web/.storybook/preview.js
@@ -1,4 +1,16 @@
+import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+const theme = {};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
