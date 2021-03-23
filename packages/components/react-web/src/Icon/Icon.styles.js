1aa0de21c26b6d84541034971ace0afef650ded2
Kendrick Arnett <kmarnett@gmail.com>
2021-03-23 15:47:34 -0500

    feat(icons): Automate creation of React icon components based on what's in `elements`
    Re: #55

--- packages/components/react-web/src/Icon/Icon.styles.js
+++ packages/components/react-web/src/Icon/Icon.styles.js
@@ -1,5 +1,5 @@
 import { css } from '@emotion/core';
 
-export default (theme) => {
+export default () => {
   return css``;
 };
--- packages/components/react-web/src/Icon/icons/Accessibility.jsx
+++
@@ -1,5 +0,0 @@
-import { Icons } from '@compassion-gds/elements';
-
-console.dir(Icons);
-
-// export
