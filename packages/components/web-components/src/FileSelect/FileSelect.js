e421e1b05dbf56038b5cb37d3bbe50566f812b29
Peter Velkov <kidroca@gmail.com>
2021-06-29 12:32:04 +0300

    feat(web-components): convert FileSelect from react-web

---
+++ packages/components/web-components/src/FileSelect/FileSelect.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reactToWebComponent from 'react-to-webcomponent';
+import { FileSelect } from '@compassion-gds/react-web';
+
+const WebFileSelect = reactToWebComponent(FileSelect, React, ReactDOM);
+
+customElements.define("gds-file-select", WebFileSelect);
---
+++ packages/components/web-components/src/FileSelect/FileSelect.stories.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { html } from 'lit-html';
+import { action } from '@storybook/addon-actions';
+
+import './FileSelect';
+
+export default {
+  title: 'Global Design System/FileSelect',
+};
+
+export const Basic = () =>  html`<gds-file-select />`;
+Basic.args = {};
+
+export const Custom =  ({ label, emptyStateLabel }) =>
+    html`<gds-file-select label=${label} .emptyStateLabel=${emptyStateLabel} />`;
+Custom.args = {
+    label: 'Custom',
+    emptyStateLabel: 'Custom Empty Label',
+    onSelect: action('On File(s) selected'),
+}
