ded3461dda2794159d594aa6ba86ea7c1bf20280
Peter Velkov <kidroca@gmail.com>
2021-06-29 12:09:59 +0300

    feat(web-components): add Storybook for web-components and a Button sample

---
+++ packages/components/web-components/src/Button/Button.stories.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { html } from 'lit-html';
+import { action } from '@storybook/addon-actions';
+
+import './Button';
+
+export default {
+  title: 'Global Design System/Button',
+  args: {
+    label: 'Button',
+    onClick: action('click handler'),
+  }
+};
+
+const Template =  ({ primary, disabled, label, onClick, size }) =>
+    html`<gds-button primary=${primary || ''} .label=${label} disabled=${disabled || ''} @click=${onClick} size=${size || ''} />`;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Primary Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Secondary Button',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  primary: true,
+  disabled: true,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+};
