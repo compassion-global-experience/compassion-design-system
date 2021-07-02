4c46430a45ed28620df3766566c04df0a780ae3f
Kendrick Arnett <kmarnett@gmail.com>
2021-07-02 11:34:45 -0500

    style(image): Linting & default prop values

--- packages/components/react-web/src/Image/Image.jsx
+++ packages/components/react-web/src/Image/Image.jsx
@@ -5,108 +5,129 @@ import PropTypes from 'prop-types';
  * UI component for images
  */
 
-export const Image = ({ src, srcPreview, srcSet, sizes, alt, caption, isLazyLoaded, width, maxWidth, height, maxHeight, objectFit, objectPosition }) => {
-    const ref = useRef(null);
-    const [currentSrc, setCurrentSrc] = useState(null);
-    const isLazyLoadedLoaded = useRef(false);
-    const currentSrcSet = useRef(null);
+export const Image = ({
+  src,
+  srcPreview,
+  srcSet,
+  sizes,
+  alt,
+  caption,
+  isLazyLoaded,
+  width,
+  maxWidth,
+  height,
+  maxHeight,
+  objectFit,
+  objectPosition,
+}) => {
+  const ref = useRef(null);
+  const [currentSrc, setCurrentSrc] = useState(null);
+  const isLazyLoadedLoaded = useRef(false);
+  const currentSrcSet = useRef(null);
 
-    useEffect(() => {
-        let isLoaded = false;
-        const { current } = ref;
+  useEffect(() => {
+    let isLoaded = false;
+    const { current } = ref;
 
-        function setSrc() {
-            if (srcPreview) {
-                setCurrentSrc(srcPreview);
+    function setSrc() {
+      if (srcPreview) {
+        setCurrentSrc(srcPreview);
 
-                let img = document.createElement('img');
-                if (srcSet) {
-                    img.srcset = src;
-                } else {
-                    img.src = src;
-                }
-                img.onload = () => {
-                    currentSrcSet.current = srcSet;
-                    setCurrentSrc(src);
-                }
-
-                isLoaded = true;
-            } else {
-                currentSrcSet.current = srcSet;
-                setCurrentSrc(src);
-                isLoaded = true;
-            }
+        const img = document.createElement('img');
+        if (srcSet) {
+          img.srcset = src;
+        } else {
+          img.src = src;
         }
+        img.onload = () => {
+          currentSrcSet.current = srcSet;
+          setCurrentSrc(src);
+        };
 
-        if (!isLoaded) {
-            if (isLazyLoaded) {
-                let observer = new IntersectionObserver(
-                    entries => entries.forEach(entry => {
-                        if (entry.isIntersecting) {
-                            setSrc();
-                            observer = observer.disconnect();
-                        }
-                    })
-                );
+        isLoaded = true;
+      } else {
+        currentSrcSet.current = srcSet;
+        setCurrentSrc(src);
+        isLoaded = true;
+      }
+    }
 
-                observer.observe(current)
-
-                return () => (observer = observer && observer.disconnect())
-            } else {
-                setSrc();
+    if (!isLoaded) {
+      if (isLazyLoaded) {
+        let observer = new IntersectionObserver((entries) =>
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              setSrc();
+              observer = observer.disconnect();
             }
-        }
-    }, [ref, src, srcPreview, srcSet, isLazyLoaded, isLazyLoadedLoaded, currentSrcSet]);
+          })
+        );
 
-    return (
-        <figure className={`img`} ref={ref}>
-            <img className={`img__photo`}
-                src={currentSrc}
-                sizes={sizes}
-                srcSet={currentSrcSet.current}
-                alt={alt}
-                style={{
-                    width: width,
-                    maxWidth: maxWidth,
-                    height: height,
-                    maxHeight: maxHeight,
-                    objectFit: objectFit,
-                    objectPosition: objectPosition
-                }} />
-            {caption &&
-                <figcaption>
-                    {caption}
-                </figcaption>
-            }
-        </figure>
-    );
-}
+        observer.observe(current);
+
+        return () => (observer = observer && observer.disconnect());
+      }
+      setSrc();
+    }
+  }, [
+    ref,
+    src,
+    srcPreview,
+    srcSet,
+    isLazyLoaded,
+    isLazyLoadedLoaded,
+    currentSrcSet,
+  ]);
+
+  return (
+    <figure className="img" ref={ref}>
+      <img
+        className="img__photo"
+        src={currentSrc}
+        sizes={sizes}
+        srcSet={currentSrcSet.current}
+        alt={alt}
+        style={{
+          width,
+          maxWidth,
+          height,
+          maxHeight,
+          objectFit,
+          objectPosition,
+        }}
+      />
+      {caption && <figcaption>{caption}</figcaption>}
+    </figure>
+  );
+};
 
 Image.defaultProps = {
-    src: null,
-    srcSet: null,
-    alt: null,
-    caption: null,
-    isLazyLoaded: true,
-    width: null,
-    maxWidth: null,
-    height: null,
-    maxHeight: null,
-    objectFit: null,
-    objectPosition: null
+  src: null,
+  srcPreview: null,
+  srcSet: null,
+  sizes: null,
+  caption: null,
+  isLazyLoaded: true,
+  width: null,
+  maxWidth: null,
+  height: null,
+  maxHeight: null,
+  objectFit: null,
+  objectPosition: null,
 };
 
 Image.propTypes = {
-    src: PropTypes.string,
-    srcSet: PropTypes.string,
-    sizes: PropTypes.string,
-    alt: PropTypes.string.isRequired,
-    caption: PropTypes.string,
-    isLazyLoaded: PropTypes.bool,
-    width: PropTypes.string,
-    maxWidth: PropTypes.string,
-    height: PropTypes.string,
-    maxHeight: PropTypes.string,
-    objectFit: PropTypes.oneOf(['unset', 'cover', 'contain', 'none', 'fill']),
-    objectPosition: PropTypes.string
+  src: PropTypes.string,
+  srcPreview: PropTypes.string,
+  srcSet: PropTypes.string,
+  sizes: PropTypes.string,
+  alt: PropTypes.string.isRequired,
+  caption: PropTypes.string,
+  isLazyLoaded: PropTypes.bool,
+  width: PropTypes.string,
+  maxWidth: PropTypes.string,
+  height: PropTypes.string,
+  maxHeight: PropTypes.string,
+  objectFit: PropTypes.oneOf(['unset', 'cover', 'contain', 'none', 'fill']),
+  objectPosition: PropTypes.string,
 };
--- packages/components/react-web/src/Image/Image.stories.js
+++ packages/components/react-web/src/Image/Image.stories.js
@@ -4,26 +4,28 @@ import '../global.css';
 import { Image } from './Image';
 
 export default {
-    title: 'Global Design System/Image',
-    component: Image,
-    argTypes: {}
+  title: 'Global Design System/Image',
+  component: Image,
+  argTypes: {},
 };
 
 const Template = (args) => <Image {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-    src: 'https://images.unsplash.com/photo-1605027628030-9bb6f83535e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=100',
-    srcPreview: 'https://images.unsplash.com/photo-1605027628030-9bb6f83535e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=10&q=100', 
-    srcSet: null, 
-    sizes: null, 
-    alt: 'Arkansas is great.', 
-    caption: null, 
-    isLazyLoaded: true, 
-    width: '500px', 
-    maxWidth: null, 
-    height: '500px', 
-    maxHeight: null, 
-    objectFit: 'cover', 
-    objectPosition: null
+  src:
+    'https://images.unsplash.com/photo-1605027628030-9bb6f83535e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=100',
+  srcPreview:
+    'https://images.unsplash.com/photo-1605027628030-9bb6f83535e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=10&q=100',
+  srcSet: null,
+  sizes: null,
+  alt: 'Arkansas is great.',
+  caption: null,
+  isLazyLoaded: true,
+  width: '500px',
+  maxWidth: null,
+  height: '500px',
+  maxHeight: null,
+  objectFit: 'cover',
+  objectPosition: null,
 };
