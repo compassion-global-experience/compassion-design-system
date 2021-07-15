65f43ba65776464c4a6535b89a17c41172d2d210
Zarko Boyadzhiev <zarkoboyadjiev@gmail.com>
2021-07-15 15:38:55 +0300

    refactor(web-components): convert Menu from react-web & add Storybook example

---
+++ packages/components/web-components/src/Menu/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reactToWebComponent from 'react-to-webcomponent';
+import { Menu } from '@compassion-gds/react-web';
+
+const WebMenu = reactToWebComponent(Menu, React, ReactDOM);
+
+customElements.define("gds-menu", WebMenu);
+ 
