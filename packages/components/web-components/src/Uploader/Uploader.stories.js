4de6c31ec5bb860450c767a34ba63375bd334f38
Zarko Boyadzhiev <zarkoboyadjiev@gmail.com>
2021-07-15 21:43:27 +0300

    refactor(web-components): convert Uploader from react-web & add Storybook example

---
+++ packages/components/web-components/src/Uploader/Uploader.stories.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { html } from 'lit-html';
+
+import './Uploader';
+
+export default {
+  title: 'Global Design System/Uploader',
+  argTypes: {
+  }
+}
+
+export const Primary = () => html`<gds-uploader />`
