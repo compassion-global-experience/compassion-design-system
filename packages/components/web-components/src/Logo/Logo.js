e4e046750892288010bb315fa08e0e2143a0b2fc
Zarko Boyadzhiev <zarkoboyadjiev@gmail.com>
2021-07-20 11:59:59 +0300

    refactor(web-components): convert Logo from react-web

---
+++ packages/components/web-components/src/Logo/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reactToWebComponent from 'react-to-webcomponent';
+import { Logo } from '@compassion-gds/react-web';
+
+const WebLogo = reactToWebComponent(Logo, React, ReactDOM);
+
+customElements.define("gds-logo", WebLogo);
