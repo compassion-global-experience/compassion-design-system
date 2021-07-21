4e49820a4846225b12b8ad0f9ab239628876c1c6
KerimRahman <kerimrahman92@gmail.com>
2021-07-21 10:37:03 +0300

    refactor(elements/space): update spacings to match the design

--- packages/elements/src/space/index.js
+++ packages/elements/src/space/index.js
@@ -10,8 +10,8 @@ const pxToRem = px => px / 16;
 /**
  * Returns a rem-based spacing.
  *
- * @param {Number} [step=0] _Must be an integer._
- *    The number of steps up or down by 1.
+ * @param {Number} [step=0]
+ *    The number of steps up or down.
  *    Defaults to the base spacing - 8px (0.5rem).
  */
 const spacing = (step) => {
@@ -29,11 +29,11 @@ export default {
   minus2: spacing(-2),
   minus1: spacing(-1),
   base: spacing(0),
-  plus1: spacing(1),
+  plus1: spacing(1.5),
   plus2: spacing(2),
   plus3: spacing(3),
   plus4: spacing(4),
   plus5: spacing(5),
   plus6: spacing(6),
-  plus7: spacing(7),
+  plus7: spacing(8),
 };
