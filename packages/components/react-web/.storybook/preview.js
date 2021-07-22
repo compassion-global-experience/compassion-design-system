aa59f618157f2513cd7968d11f25bc0376aedc68
KerimRahman <kerimrahman92@gmail.com>
2021-07-22 21:06:06 +0300

    refactor(packages): extract to reset.css and load them in react/web storybook

--- packages/components/react-web/.storybook/preview.js
+++ packages/components/react-web/.storybook/preview.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from '../src';
 
+import '!style-loader!css-loader!../../../reset.css';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   backgrounds: { grid: { cellSize: 16, opacity: 0.25 } },
