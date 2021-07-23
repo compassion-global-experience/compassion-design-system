0b1a8ac81b8891995cda41f385140018e3a4029c
KerimRahman <kerimrahman92@gmail.com>
2021-07-23 16:26:40 +0300

    Revert "refactor(packages): extract to reset.css and load them in react/web storybook"
    This reverts commit aa59f618

--- packages/components/react-web/.storybook/preview.js
+++ packages/components/react-web/.storybook/preview.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from '../src';
 
-import '!style-loader!css-loader!../../../reset.css';
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   backgrounds: { grid: { cellSize: 16, opacity: 0.25 } },
--- packages/components/web-components/.storybook/preview.js
+++ packages/components/web-components/.storybook/preview.js
@@ -1,5 +1,3 @@
-import '!style-loader!css-loader!../../../reset.css';
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   backgrounds: { grid: { cellSize: 16, opacity: 0.25 } },
