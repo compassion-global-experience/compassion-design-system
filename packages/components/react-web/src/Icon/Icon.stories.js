b2bc102752b9c656f8383ba2df5570e7dfa9bb15
Peter Velkov <kidroca@gmail.com>
2021-08-24 20:21:54 +0300

    refactor: list all React Icons as well

--- packages/components/react-web/src/Icon/Icon.stories.js
+++ packages/components/react-web/src/Icon/Icon.stories.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { icons } from '@compassion-gds/elements';
+
 import IconSkippy from './icons/IconSkippy';
+import * as Icons from './index';
 
 export default {
   title: 'Global Design System/Icon',
   component: IconSkippy,
-  argTypes: {},
+  args: {
+    sampleIcons: Object.keys(icons),
+    color: '#005EB8',
+  },
+  argTypes: {
+      color: {
+          control: 'color',
+      }
+  }
 };
 
-const Template = (args) => <IconSkippy {...args} />;
+export const SampleVariants = (args) => (
+  <ul style={{ display: 'flex', flexWrap: 'wrap', listStyle: 'none' }}>
+    {args.sampleIcons.map(name => {
+      const iconName = `Icon${name[0].toUpperCase()}${name.substr(1)}`;
+      const Icon = Icons[iconName];
 
-export const Primary = Template.bind({});
-Primary.args = {};
+      return (
+          <li key="name" style={{ width: 222, display: 'flex', alignItems: 'center', gap: 8 }}>
+            <Icon width="2em" height="2em" fill={args.color} /> <span>{name}</span>
+          </li>
+      );
+    })}
+  </ul>
+);
