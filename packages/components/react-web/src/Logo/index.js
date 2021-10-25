b91150d65d9a148354188cb3a74af3bb7d929f59
Peter Velkov <kidroca@gmail.com>
2021-10-25 22:24:35 +0300

    refactor(react-web): export Logo and fix web-components logo
    The prop name `style` is problematic as the converted web-component tries
    to pass it to the html element as `style` attribute
    It was changed to `type` which seems to work

--- packages/components/react-web/src/Logo/index.js
+++ packages/components/react-web/src/Logo/index.js
@@ -1,2 +1,3 @@
 export { default as LogoFull } from './logos/LogoFull';
 export { default as LogoMinimal } from './logos/LogoMinimal';
+export { Logo } from './Logo';
--- packages/components/web-components/src/Logo/Logo.js
+++ packages/components/web-components/src/Logo/Logo.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reactToWebComponent from 'react-to-webcomponent';
-import { LogoFull, LogoMinimal } from '@compassion-gds/react-web';
+import { Logo } from '@compassion-gds/react-web';
 
-const WebLogoFull = reactToWebComponent(LogoFull, React, ReactDOM);
-const WebLogoMinimal = reactToWebComponent(LogoMinimal, React, ReactDOM);
+const WebLogo = reactToWebComponent(Logo, React, ReactDOM);
 
-customElements.define('gds-logo-full', WebLogoFull);
-customElements.define('gds-logo-minimal', WebLogoMinimal);
+customElements.define('gds-logo', WebLogo);
