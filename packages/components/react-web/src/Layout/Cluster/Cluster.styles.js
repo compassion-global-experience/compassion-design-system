e743cddeb8f1cce76c88630e37a2e492b86e3058
Zarko Boyadzhiev <zarkoboyadjiev@gmail.com>
2021-11-01 17:42:41 +0200

    refactor(react-web/Cluster): styles refactoring

--- packages/components/react-web/src/Layout/Cluster/Cluster.styles.js
+++ packages/components/react-web/src/Layout/Cluster/Cluster.styles.js
@@ -1,24 +1,26 @@
 import { css } from '@emotion/react';
 
 const justifyContentPartial = (justifyContent) => {
-  return justifyContent === 'flex-start'
-    ? css``
-    : css`
-        justify-content: ${justifyContent};
-      `;
+  return (
+    justifyContent !== 'flex-start' &&
+    css`
+      justify-content: ${justifyContent};
+    `
+  );
 };
 
 const alignItemsPartial = (alignItems) => {
-  return alignItems === 'flex-start'
-    ? css``
-    : css`
-        align-items: ${alignItems};
-      `;
+  return (
+    alignItems !== 'flex-start' &&
+    css`
+      align-items: ${alignItems};
+    `
+  );
 };
 
 export default (theme) => {
   return css`
-    --gds-cluster-space: ${theme.space === `0` ? `0px` : theme.space};
+    --gds-cluster-space: ${theme.space ?? '0'};
     overflow: hidden;
 
     & > * {
