d5342286b0705a39d47674b65848f81f69428775
Zarko Boyadzhiev <zarkoboyadjiev@gmail.com>
2021-11-01 17:05:43 +0200

    refactor(react-web/Center): styles refactoring

--- packages/components/react-web/src/Layout/Center/Center.styles.js
+++ packages/components/react-web/src/Layout/Center/Center.styles.js
@@ -1,32 +1,35 @@
 import { css } from '@emotion/react';
 
 const maxWidthPartial = (maxWidth) => {
-  return maxWidth !== 0
-    ? css`
-        --gds-center-measure: ${maxWidth};
-        max-width: var(--gds-center-measure);
-      `
-    : css``;
+  return (
+    maxWidth &&
+    css`
+      --gds-center-measure: ${maxWidth};
+      max-width: var(--gds-center-measure);
+    `
+  );
 };
 
 const gutterPartial = (gutter) => {
-  return gutter !== 0
-    ? css`
-        box-sizing: content-box;
-        padding-right: ${gutter};
-        padding-left: ${gutter};
-      `
-    : css``;
+  return (
+    gutter &&
+    css`
+      box-sizing: content-box;
+      padding-right: ${gutter};
+      padding-left: ${gutter};
+    `
+  );
 };
 
 const intrinsicallyCenterPartial = (intrinsicallyCenter) => {
-  return intrinsicallyCenter
-    ? css`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      `
-    : css``;
+  return (
+    intrinsicallyCenter &&
+    css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    `
+  );
 };
 
 export default (theme) => {
