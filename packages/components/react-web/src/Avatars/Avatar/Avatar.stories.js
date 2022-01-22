4dbef88641e28560cbd52a43de0a166e74b70138
Peter Velkov <kidroca@gmail.com>
2022-01-22 19:23:40 +0200

    story(react-web/Image): remove image lazy loading to fix percy snapshot

--- packages/components/react-web/src/Avatars/Avatar/Avatar.stories.js
+++ packages/components/react-web/src/Avatars/Avatar/Avatar.stories.js
@@ -22,7 +22,6 @@ const imageComponent = (
     alt="Arkansas is great."
     objectFit="cover"
     src="https://images.unsplash.com/photo-1605027628030-9bb6f83535e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=64&q=100"
-    srcPreview="https://images.unsplash.com/photo-1605027628030-9bb6f83535e6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=10&q=100"
   />
 );
 
