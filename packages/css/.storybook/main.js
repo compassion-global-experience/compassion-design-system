27230d3f07d573060639be29fe10affbc2c6df00
Kerim Rahman <krahman@cytec.bg>
2022-12-13 18:12:19 +0200

    chore(packages/css): add storybook

---
+++ packages/css/.storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@etchteam/storybook-addon-css-variables-theme"
+  ],
+  "framework": "@storybook/html"
+}
---
+++ packages/css/.storybook/preview.js
@@ -0,0 +1,25 @@
+import light from '!!style-loader?injectType=lazyStyleTag!css-loader!../vars/_cds-light.css'
+import dark from '!!style-loader?injectType=lazyStyleTag!css-loader!../vars/_cds-dark.css'
+
+import cssVariablesTheme from '@etchteam/storybook-addon-css-variables-theme'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  cssVariables: {
+    files: {
+      'Light Theme': light,
+      'Dark Theme': dark,
+    },
+    defaultTheme: 'Light Theme'
+  }
+}
+
+export const decorators = [
+  cssVariablesTheme,
+];
