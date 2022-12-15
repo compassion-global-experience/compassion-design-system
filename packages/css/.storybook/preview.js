157664205e470d9169c86085b0959db15049f195
Kerim Rahman <krahman@cytec.bg>
2022-12-15 17:27:32 +0200

    refactor(css): extract `reset.css` for storybook preview

--- packages/css/.storybook/preview.js
+++ packages/css/.storybook/preview.js
@@ -1,3 +1,5 @@
+import '../reset.css'
+
 import light from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/theme/light.css'
 import dark from '!!style-loader?injectType=lazyStyleTag!css-loader!../src/theme/dark.css'
 
