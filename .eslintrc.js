494c99c95e83308b7e544e3cdb5d1ddea4a7df08
Peter Velkov <kidroca@gmail.com>
2023-01-28 19:06:03 +0200

    chore: update eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,15 @@
 module.exports = {
-  extends: ['airbnb', 'standard', 'plugin:prettier/recommended'],
   env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
+    es2021: true,
     node: true,
   },
-  rules: {
-    'import/prefer-default-export': ['off'],
-    'max-len': [
-      'warn',
-      {
-        code: 80,
-        tabWidth: 2,
-        comments: 80,
-        ignoreComments: true,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-      },
-    ],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'object-curly-spacing': ['warn', 'always'],
-    'space-in-parens': ['warn', 'never'],
+  extends: ['standard', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
-  ignorePatterns: ['**/dist/**/*'],
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
 };
