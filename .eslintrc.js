d1bd9d318d5a745c3707bdfec8511a5ff7653e86
Peter Velkov <kidroca@gmail.com>
2023-02-05 15:01:12 +0200

    chore: setup eslint for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'prettier'],
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -12,4 +15,5 @@ module.exports = {
     semi: ['error', 'always'],
     'comma-dangle': ['error', 'always-multiline'],
   },
+  ignorePatterns: ['dist/', 'storybook-static/'],
 };
