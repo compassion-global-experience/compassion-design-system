a35053093d0ccc94cbec95886533561d301aac44
Kerim Rahman <krahman@cytec.bg>
2023-02-21 12:16:09 +0200

    feat(css/Form): add PIN field styles/stories

---
+++ packages/css/src/components/Form/PIN.stories.js
@@ -0,0 +1,63 @@
+export default {
+    title: 'Components/Forms/PIN',
+    argTypes: {
+        label: { control: 'text' },
+        size: {
+            control: { type: 'select' },
+            options: ['small', 'medium', 'large'],
+        },
+    },
+};
+
+const PinField = ({
+  label = 'Label Text',
+  size = 'medium',
+  state = 'default',
+}) => {
+    const disabled = state === 'disabled' ? 'disabled' : '';
+
+    return `
+    <div class="form-field-row">
+      <div class="form-label-wrap">
+        <label class="form-label ${disabled}" for="test-field">
+          ${label}
+        </label>
+      </div>
+      <div class="pin-field ${size}">
+        <div class="form-field ${disabled}">
+          <input class="form-input" type="password" maxlength=1 ${disabled} />
+        </div>
+        <div class="form-field ${disabled}">
+          <input class="form-input" type="password" maxlength=1 ${disabled} />
+        </div>
+        <div class="form-field ${disabled}">
+          <input class="form-input" type="password" maxlength=1 ${disabled} />
+        </div>
+        <div class="form-field ${disabled}">
+          <input class="form-input" type="password" maxlength=1 ${disabled} />
+        </div>
+      </div>
+    </div>
+    
+    <script>
+      var inputs = document.querySelectorAll("input");
+      inputs.forEach((input, key) => {
+        if (key !== 0) {
+          input.addEventListener("click", function () {
+            inputs[0].focus();
+          });
+        }
+        input.addEventListener("keyup", function () {
+          if (input.value) {
+            inputs[key + 1].focus();
+          }
+        });
+      });
+    </script>
+`;
+};
+
+export const Default = PinField.bind({});
+Default.args = {
+    label: 'Label Text',
+};
