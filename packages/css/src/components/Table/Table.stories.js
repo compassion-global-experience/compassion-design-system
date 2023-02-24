162f3a788467639ea60eace7ea2c9d0dd9bd3983
Kerim R <kerimrahman92@gmail.com>
2023-02-24 15:23:13 +0200

    Table styles/stories (#147)
    * feat(css): add Table styles/stories
    
    * refactor(css/Table.stories): optional prop
    
    * refactor(css/table): replace `th` usage
    
    * refactor(css/table): option to partially disable
    
    ---------
    
    Co-authored-by: Kerim Rahman <krahman@cytec.bg>

---
+++ packages/css/src/components/Table/Table.stories.js
@@ -0,0 +1,106 @@
+export default {
+  title: 'Components/Table',
+  argTypes: {
+    stickyHeader: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+    state: {
+      control: { type: 'select' },
+      options: ['default', 'disabled', 'active'],
+    },
+  },
+  decorators: [(Story) => `<div style="width: 450px;">${Story()}</div>`],
+};
+
+const Table = ({
+  stickyHeader = false,
+  state = 'default',
+  tableWidth = null,
+  containerHeight = null,
+  disabledRow = false,
+  disabledCell = false,
+}) => {
+  const sticky = stickyHeader ? 'sticky-header' : '';
+
+  return `
+    <div class="table-container" style="${containerHeight ? `max-height: ${containerHeight}` : ''}">
+      <table class="table ${sticky} ${state}" style="${tableWidth ? `width: ${tableWidth}` : ''}">
+      <thead class="table-head">
+        <tr class="table-row">
+          <th class="table-cell ${state}">Heading</th>
+          <th class="table-cell ${state}"><a href="https://google.com">Link</a></th>
+          <th class="table-cell ${state}">Heading</th>
+        </tr>
+      </thead>
+      <tbody class="table-body">
+        <tr class="table-row ${disabledRow ? 'disabled' : ''}">
+          <td class="table-cell ${disabledCell ? 'disabled' : ''}">Cell 1</td>
+          <td class="table-cell">Cell 2</td>
+          <td class="table-cell">Cell 3</td>
+        </tr>
+        <tr class="table-row">
+          <td class="table-cell"><a href="https://google.com">Link 1</a></td>
+          <td class="table-cell"><a href="https://google.com">Link 2</a></td>
+          <td class="table-cell"><a href="https://google.com">Link 3</a></td>
+        </tr>
+        <tr class="table-row">
+          <td class="table-cell">Cell 1</td>
+          <td class="table-cell">Cell 2</td>
+          <td class="table-cell">Cell 3</td>
+        </tr>
+        <tr class="table-row">
+          <td class="table-cell"><a href="https://google.com">Link 1</a></td>
+          <td class="table-cell"><a href="https://google.com">Link 2</a></td>
+          <td class="table-cell"><a href="https://google.com">Link 3</a></td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+`;
+};
+
+export const Default = Table.bind({});
+Default.args = {
+  state: 'default',
+  stickyHeader: false,
+};
+
+export const Disabled = Table.bind({});
+Disabled.args = {
+  state: 'disabled',
+  stickyHeader: false,
+};
+
+export const DisabledFirstRow = Table.bind({});
+DisabledFirstRow.args = {
+  state: 'default',
+  stickyHeader: false,
+  disabledRow: true,
+};
+
+export const DisabledFirstCell = Table.bind({});
+DisabledFirstCell.args = {
+  state: 'default',
+  stickyHeader: false,
+  disabledCell: true,
+};
+
+export const HorizontalScroll = Table.bind({});
+HorizontalScroll.args = {
+  state: 'default',
+  tableWidth: '500px',
+};
+
+export const StickyHeader = Table.bind({});
+StickyHeader.args = {
+  state: 'default',
+  stickyHeader: true,
+  containerHeight: '150px',
+};
+
+export const HorizontalAndSticky = Table.bind({});
+HorizontalAndSticky.args = {
+  state: 'default',
+  stickyHeader: true,
+  containerHeight: '150px',
+  tableWidth: '500px',
+};
