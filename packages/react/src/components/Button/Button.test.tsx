02431bd4c8477373da0a6ce469794d11e56b144c
Kerim Rahman <krahman@cytec.bg>
2023-03-01 19:05:44 +0200

    test(react/Button): compare with the snapshot

--- packages/react/src/components/Button/Button.test.tsx
+++ packages/react/src/components/Button/Button.test.tsx
@@ -5,41 +5,21 @@ import Button from './Button';
 describe('Button', () => {
   it('should render `Primary` mode with `Medium` size', () => {
     const { container } = render(<Button label="Click Me!" mode="primary" />);
-    const classNames = ['primary', 'medium'];
-
-    const result = classNames.every(className => {
-      return container.getElementsByClassName(className);
-    });
-
-    expect(result).toBe(true);
+    expect(container).toMatchSnapshot();
   });
 
   it('should render `Secondary` mode with `Small` size', () => {
     const { container } = render(<Button label="Click Me!" mode="secondary" size="small" />);
-    const classNames = ['secondary', 'small'];
-
-    const result = classNames.every(className => {
-      return container.getElementsByClassName(className);
-    });
-
-    expect(result).toBe(true);
+    expect(container).toMatchSnapshot();
   });
 
   it('should render `Tertiary` mode with `Large` size', () => {
     const { container } = render(<Button label="Click Me!" mode="tertiary" size="large" />);
-    const classNames = ['tertiary', 'large'];
-
-    const result = classNames.every(className => {
-      return container.getElementsByClassName(className);
-    });
-
-    expect(result).toBe(true);
+    expect(container).toMatchSnapshot();
   });
 
   it('should render `Disabled` variant', () => {
     const { container } = render(<Button label="Disabled" mode="primary" disabled />);
-    const button = container.getElementsByTagName('button')[0];
-
-    expect(button.disabled).toBe(true);
+    expect(container).toMatchSnapshot();
   });
 });
