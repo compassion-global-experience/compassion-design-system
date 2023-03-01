93825ec8d1ef9f9e052947e63d7d0c10565760c3
Kerim Rahman <krahman@cytec.bg>
2023-03-01 15:38:04 +0200

    test(react/Button): add test cases

--- packages/react/src/components/Button/Button.test.tsx
+++ packages/react/src/components/Button/Button.test.tsx
@@ -3,7 +3,43 @@ import { render } from '@testing-library/react';
 import Button from './Button';
 
 describe('Button', () => {
-  test('renders the Button component', () => {
-    render(<Button label="Hello world!" />);
+  it('should render `Primary` mode with `Medium` size', () => {
+    const { container } = render(<Button label="Click Me!" mode="primary" />);
+    const classNames = ['primary', 'medium'];
+
+    const result = classNames.every(className => {
+      return container.getElementsByClassName(className);
+    });
+
+    expect(result).toBe(true);
+  });
+
+  it('should render `Secondary` mode with `Small` size', () => {
+    const { container } = render(<Button label="Click Me!" mode="secondary" size="small" />);
+    const classNames = ['secondary', 'small'];
+
+    const result = classNames.every(className => {
+      return container.getElementsByClassName(className);
+    });
+
+    expect(result).toBe(true);
+  });
+
+  it('should render `Tertiary` mode with `Large` size', () => {
+    const { container } = render(<Button label="Click Me!" mode="tertiary" size="large" />);
+    const classNames = ['tertiary', 'large'];
+
+    const result = classNames.every(className => {
+      return container.getElementsByClassName(className);
+    });
+
+    expect(result).toBe(true);
+  });
+
+  it('should render `Disabled` variant', () => {
+    const { container } = render(<Button label="Disabled" mode="primary" disabled />);
+    const button = container.getElementsByTagName('button')[0];
+
+    expect(button.disabled).toBe(true);
   });
 });
