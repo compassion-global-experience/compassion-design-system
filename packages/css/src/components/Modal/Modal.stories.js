166c99eea2ed60dbf891f5a7e78971c2ccd06374
Kerim Rahman <krahman@cytec.bg>
2023-03-02 15:09:47 +0200

    refactor(css/Modal.stories): update default state Allow Visual Regression testing tool to capture a picture of the modal

--- packages/css/src/components/Modal/Modal.stories.js
+++ packages/css/src/components/Modal/Modal.stories.js
@@ -1,6 +1,5 @@
 export default {
   title: 'Components/Modal',
-  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
     header: { control: 'boolean' },
     footer: { control: 'boolean' },
@@ -20,14 +19,14 @@ const Template = ({
 }) => {
   return `
     <button type="button" class="button-main secondary medium" onclick='openModal()'>Open Modal</button>
-    <span id="example-backdrop" style="display: none; position: fixed; left: 0; right: 0; top: 0; bottom: 0; background: black; opacity: 0.7; "></span>
-    <div style="display: none;" id="example-modal" class="modal ${size}">
+    <span id="example-backdrop" style="position: fixed; inset: 0; background: black; opacity: 0.7; "></span>
+    <div id="example-modal" class="modal ${size}">
         <span id="example-modal-close" class="modal-button-close" onclick='closeModal()'>
-            <i class="ph-x ph-xl"></i>
+          <i class="ph-x ph-xl"></i>
         </span>
         ${renderHeader({ header, title })}
         <div class="modal-body">
-            <div style="border: 3px dashed; text-align: center; border-radius: 4px; margin: 10px; padding: 10px;">Slot</div>
+          <div style="border: 3px dashed; text-align: center; border-radius: 4px; margin: 10px; padding: 10px;">Slot</div>
         </div>
         ${renderFooter({ footer })}
     </div>
@@ -56,11 +55,11 @@ const renderFooter = ({ footer }) => {
   if (!footer) return '';
 
   return `
-        <div class="modal-footer">
-            <button type="button" class="button-main secondary medium">Button 2</button>
-            <button type="button" class="button-main primary medium">Button 1</button>
-        </div>
-        `;
+    <div class="modal-footer">
+      <button type="button" class="button-main secondary medium">Button 2</button>
+      <button type="button" class="button-main primary medium">Button 1</button>
+    </div>
+    `;
 };
 
 export const Default = Template.bind({});
