ffa885bd637ac4fab15a20f3aaa15ffd74afe4a3
Peter Velkov <kidroca@gmail.com>
2023-03-02 14:46:06 +0200

    Examples vanilla (#151)
    * chore(examples/vanilla): turn old web-components example into vanilla/css example
    
    * refactor(examples/vanilla): move styles to css, add logo
    
    * refactor(css/form): update margin usage
    
    * refactor(examples/vanilla): code refactoring
    
    ---------
    
    Co-authored-by: Kerim Rahman <krahman@cytec.bg>

---
+++ examples/vanilla/webpack.config.cjs
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env) =>
+  ({
+    mode: env.WEBPACK_SERVE ? 'development' : 'production',
+    entry: './src/index.js',
+    devtool: env.WEBPACK_SERVE ? 'inline-source-map' : 'source-map',
+    devServer: {
+      static: ['./dist', './assets'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[hash][ext][query]',
+          },
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[hash][ext][query]',
+          },
+        },
+      ],
+    },
+  });
