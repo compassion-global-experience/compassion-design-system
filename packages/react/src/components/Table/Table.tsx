4daf624773c86a8b7324f33844484847f1b378ff
Peter Velkov <kidroca@gmail.com>
2023-03-09 21:16:24 +0200

    refactor(packages/react): add generic row type for table

--- packages/react/src/components/Table/Table.tsx
+++ packages/react/src/components/Table/Table.tsx
@@ -1,24 +1,32 @@
 import { ReactElement, CSSProperties } from 'react';
 import '@compassion-gds/css/src/components/Table/table.css';
 
-export interface Column {
+export interface Column<Row> {
   title: string;
   key: string;
-  headerRender?: (col: Column) => ReactElement;
-  cellRender?: (col: Column, data: object) => ReactElement;
+  headerRender?: (col: Column<Row>) => ReactElement;
+  cellRender?: (col: Column<Row>, data: Row) => ReactElement;
 }
 
-export interface TableProps {
-  columns: Column[],
-  rows: object[],
-  disabled?: boolean,
-  stickyHeader?: boolean,
-  containerStyle?: CSSProperties,
-  tableStyle?: CSSProperties,
-  className?: string,
+export interface TableProps<Row> {
+  columns: Column<Row>[];
+  rows: Row[];
+  disabled?: boolean;
+  stickyHeader?: boolean;
+  containerStyle?: CSSProperties;
+  tableStyle?: CSSProperties;
+  className?: string;
 }
 
-const Table = ({ columns = [], rows = [], disabled = false, stickyHeader = false, containerStyle, tableStyle, className }: TableProps) => {
+const Table = <Row,>({
+  columns = [],
+  rows = [],
+  disabled = false,
+  stickyHeader = false,
+  containerStyle,
+  tableStyle,
+  className,
+}: TableProps<Row>) => {
   const sticky = stickyHeader ? 'sticky-header' : '';
   const disabledTable = disabled ? 'disabled' : '';
   const classNames = ['table', sticky, disabledTable].join(' ');
@@ -29,25 +37,29 @@ const Table = ({ columns = [], rows = [], disabled = false, stickyHeader = false
       <table className={classNames} style={tableStyle}>
         <thead className="table-head">
           <tr className="table-row">
-            {
-              columns.map(col => (
-                col.headerRender
-                  ? col.headerRender(col)
-                  : <th key={col.key} className="table-cell">{col.title}</th>
-              ))
-            }
+            {columns.map((col) =>
+              col.headerRender ? (
+                col.headerRender(col)
+              ) : (
+                <th key={col.key} className="table-cell">
+                  {col.title}
+                </th>
+              ),
+            )}
           </tr>
         </thead>
         <tbody className="table-body">
           {rows.map((row, rowIndex) => (
             <tr key={rowIndex} className="table-row">
-              {
-                columns.map((col) => (
-                  col.cellRender
-                    ? col.cellRender(col, row)
-                    : <td className="table-cell" key={`${rowIndex}-${col.key}`}>{row[col.key]}</td>
-                ))
-              }
+              {columns.map((col) =>
+                col.cellRender ? (
+                  col.cellRender(col, row)
+                ) : (
+                  <td className="table-cell" key={`${rowIndex}-${col.key}`}>
+                    {row[col.key]}
+                  </td>
+                ),
+              )}
             </tr>
           ))}
         </tbody>
