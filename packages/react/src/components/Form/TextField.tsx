7f393a1244bd3f37a909bef9d5bed942ce016b3a
Peter Velkov <kidroca@gmail.com>
2023-03-09 20:58:14 +0200

    refactor(packages/react): forward ref for TextField

--- packages/react/src/components/Form/TextField.tsx
+++ packages/react/src/components/Form/TextField.tsx
@@ -1,33 +1,47 @@
 import '@compassion-gds/css/src/components/Form/common.css';
 
 import Input, { InputProps } from './Input';
+import { forwardRef } from 'react';
 
 export interface TextFieldProps extends InputProps {
   label?: string;
-  hint?: string,
-  fieldClassName?: string,
+  hint?: string;
+  fieldClassName?: string;
 }
 
-const TextField = ({
-  id,
-  label = '',
-  state,
-  hint = '',
-  className,
-  fieldClassName,
-  ...rest
-}: TextFieldProps) => {
-  const containerClassNames =  ['form-field-row', className].join(' ');
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>((props, ref) => {
+  const {
+    id,
+    label = '',
+    state,
+    hint = '',
+    className,
+    fieldClassName,
+    ...rest
+  } = props;
+  const containerClassNames = ['form-field-row', className].join(' ');
   const labelClassNames = ['form-label', state].join(' ');
   const helperClassNames = ['form-hint', state].join(' ');
 
   return (
     <div className={containerClassNames}>
-      {label && <label className={labelClassNames} htmlFor={id}>{label}</label>}
-      <Input id={id} state={state} className={fieldClassName} {...rest} />
+      {label && (
+        <label className={labelClassNames} htmlFor={id}>
+          {label}
+        </label>
+      )}
+      <Input
+        ref={ref}
+        id={id}
+        state={state}
+        className={fieldClassName}
+        {...rest}
+      />
       {hint && <span className={helperClassNames}>{hint}</span>}
     </div>
   );
-};
+});
+
+TextField.displayName = 'TextField';
 
 export default TextField;
