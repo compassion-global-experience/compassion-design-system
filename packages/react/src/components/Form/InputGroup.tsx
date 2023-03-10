9ed0dc80d8e1217566c4607deca5a5983446fb51
Kerim Rahman <krahman@cytec.bg>
2023-03-10 14:25:32 +0200

    refactor(react/InputGroup): reuse `InputGroupContainer`

--- packages/react/src/components/Form/InputGroup.tsx
+++ packages/react/src/components/Form/InputGroup.tsx
@@ -1,47 +1,50 @@
-import { ReactElement } from 'react';
+import { forwardRef } from 'react';
 import '@compassion-gds/css/src/components/Form/input-group.css';
 
+import Input from './Input';
 import { TextFieldProps } from './TextField';
-import { FieldContainer, Input } from './FormElements';
-import { Info } from '../icons';
+import { InputGroupContainer, AdornmentProps } from './Helpers';
 
-export interface InputGroupProps extends TextFieldProps {
-  adornmentIcon?: ReactElement,
-  adornmentPosition?: 'start' | 'end',
-  adornmentText?: string,
-}
+export interface InputGroupProps extends TextFieldProps, AdornmentProps {}
 
-const InputGroup = ({
-  id,
-  label = '',
-  state,
-  hint = '',
-  adornmentPosition = 'end',
-  adornmentText = '',
-  adornmentIcon,
-  className,
-  fieldClassName,
-  size,
-  ...rest
-}: InputGroupProps) => {
-  const startPosition = adornmentPosition === 'start';
+const InputGroup =  forwardRef<HTMLInputElement, InputGroupProps>((props, ref) => {
+  const {
+    id,
+    label = '',
+    state,
+    size,
+    hint = '',
+    adornmentPosition = 'end',
+    adornmentText = '',
+    adornmentIcon,
+    className,
+    fieldClassName,
+    ...rest
+  } = props;
 
   return (
-    <FieldContainer id={id} label={label} state={state} hint={hint} className={className}>
-      <div className="input-group">
-        {startPosition && <Adornment icon={adornmentIcon} text={adornmentText} size={size} />}
-        <Input id={id} state={state} className={fieldClassName} size={size} {...rest} />
-        {!startPosition && <Adornment icon={adornmentIcon} text={adornmentText} size={size} />}
-      </div>
-    </FieldContainer>
+    <InputGroupContainer
+      id={id}
+      label={label}
+      state={state}
+      hint={hint}
+      className={className}
+      adornmentPosition={adornmentPosition}
+      adornmentText={adornmentText}
+      adornmentIcon={adornmentIcon}
+    >
+      <Input
+        ref={ref}
+        id={id}
+        state={state}
+        size={size}
+        className={fieldClassName}
+        {...rest}
+      />
+    </InputGroupContainer>
   );
-};
+});
 
-const Adornment = ({ icon, text, size }: { icon: ReactElement, text: string, size: string }) => (
-  <div className={['input-group-adornment', size].join(' ')}>
-    {icon || <Info size={22} />}
-    {text && <span className="input-group-adornment-text">{text}</span>}
-  </div>
-);
+InputGroup.displayName = 'InputGroup';
 
 export default InputGroup;
