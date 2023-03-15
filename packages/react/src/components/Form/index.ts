e5e55c22bd08df871bc9d3e53a7c9ee7119785ba
Kerim Rahman <krahman@cytec.bg>
2023-03-15 15:23:53 +0200

    refactor(react/components): export `Select`

--- packages/react/src/components/Form/index.ts
+++ packages/react/src/components/Form/index.ts
@@ -2,5 +2,6 @@ export { default as Input } from './Input';
 export { default as TextField } from './TextField';
 export { default as InputGroup } from './InputGroup';
 export { default as TextArea } from './TextArea';
+export { default as Select } from './Select';
 export { default as SelectField } from './SelectField';
 export { default as SelectGroup } from './SelectGroup';
--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './Button';
 export { default as Modal } from './Modal';
-export { Input, InputGroup, TextField, TextArea, SelectField, SelectGroup } from './Form';
+export { Input, InputGroup, TextField, TextArea, Select, SelectField, SelectGroup } from './Form';
