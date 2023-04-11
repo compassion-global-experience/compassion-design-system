03b446d3ba9654e1bdbb5a17a6f8c05361e75b1e
kidroca <kidroca@users.noreply.github.com>
2023-04-11 08:58:24 +0000

    chore: build packages

--- packages/react/dist/esm/types/components/Button/Button.d.ts
+++ packages/react/dist/esm/types/components/Button/Button.d.ts
@@ -1,10 +1,16 @@
-import { MouseEvent, ButtonHTMLAttributes } from 'react';
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    label: string;
+import { MouseEvent, ButtonHTMLAttributes, ReactNode } from 'react';
+export interface ButtonAttr extends ButtonHTMLAttributes<HTMLButtonElement> {
     mode?: 'primary' | 'secondary' | 'tertiary';
     size?: 'small' | 'medium' | 'large';
     disabled?: boolean;
     onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
+export type ButtonProps = ButtonAttr & ({
+    label: ReactNode;
+    children?: never;
+} | {
+    children: ReactNode;
+    label?: never;
+});
 declare const Button: import("react").ForwardRefExoticComponent<ButtonProps & import("react").RefAttributes<HTMLButtonElement>>;
 export default Button;
