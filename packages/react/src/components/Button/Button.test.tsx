0beeadfe1e09c9339e44e1c8d93842462290bcae
Peter Velkov <kidroca@gmail.com>
2023-04-11 15:04:57 +0300

    test(react): Add tests for button with icon

--- packages/react/src/components/Button/Button.test.tsx
+++ packages/react/src/components/Button/Button.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 
 import Button from './Button';
+import { Icon } from '../';
 
 describe('Button', () => {
   it('should render `Primary` mode with `Medium` size', () => {
@@ -9,17 +10,50 @@ describe('Button', () => {
   });
 
   it('should render `Secondary` mode with `Small` size', () => {
-    const { container } = render(<Button label="Click Me!" mode="secondary" size="small" />);
+    const { container } = render(
+      <Button label="Click Me!" mode="secondary" size="small" />,
+    );
     expect(container).toMatchSnapshot();
   });
 
   it('should render `Tertiary` mode with `Large` size', () => {
-    const { container } = render(<Button label="Click Me!" mode="tertiary" size="large" />);
+    const { container } = render(
+      <Button label="Click Me!" mode="tertiary" size="large" />,
+    );
     expect(container).toMatchSnapshot();
   });
 
   it('should render `Disabled` variant', () => {
-    const { container } = render(<Button label="Disabled" mode="primary" disabled />);
+    const { container } = render(
+      <Button label="Disabled" mode="primary" disabled />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Should render a variant with a left icon', () => {
+    const { container } = render(
+      <Button>
+        <Icon.ArrowLeft /> Click Me!
+      </Button>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Should render a variant with a right icon', () => {
+    const { container } = render(
+      <Button>
+        Click Me! <Icon.ArrowRight />
+      </Button>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Should render a variant with a left and right icon', () => {
+    const { container } = render(
+      <Button>
+        <Icon.ArrowLeft /> Click Me! <Icon.ArrowRight />
+      </Button>,
+    );
     expect(container).toMatchSnapshot();
   });
 });
