da3421e7562f1fbec08b965a246ab6a4f9af5e21
Kendrick Arnett <kmarnett@gmail.com>
2023-08-11 14:07:30 -0500

    Add 'This is generated' header to built CSS modules.

--- packages/core/scripts/build-sass.sh
+++ packages/core/scripts/build-sass.sh
@@ -2,5 +2,12 @@
 
 for SCSS in src/components/**/*.scss; do
   OUTPUT=${SCSS%.scss}.module.css
-  sass $SCSS $OUTPUT
+  HEADER="\
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */\n\
+/* THIS IS GENERATED CODE. Any changes you make will be overwritten! */\n\
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */\n"
+  echo "$HEADER" > "$OUTPUT"
+
+  sass "$SCSS" >> "$OUTPUT"
+  echo "Wrote $SCSS to $OUTPUT."
 done
--- packages/core/scripts/watch-sass.js
+++ packages/core/scripts/watch-sass.js
@@ -10,6 +10,12 @@ const watcher = chokidar.watch('**/*.scss', {
 
 watcher.on('change', (changedFile) => {
   let outputPath = changedFile.replace('.scss', '.module.css');
-  fs.writeFileSync(outputPath, sass.compile(changedFile).css);
+
+  const header =
+    '/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */\n\
+/* THIS IS GENERATED CODE. Any changes you make will be overwritten! */\n\
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */\n\n';
+
+  fs.writeFileSync(outputPath, header + sass.compile(changedFile).css);
   console.dir(`\n\nWrote ${outputPath}.\n`);
 });
