486b6322e62f0d372a4086e587f7cc99a4a28c27
Jane Hur <janehur8@gmail.com>
2023-08-24 10:09:50 -0500

    NavItem Component (#188)
    * Core navItem
    
    * React navItem
    
    * Incorporate namespacing changes
    
    * Update Jest snapshot tests
    
    * feat(navitem component): Adds NavItem component
    
    * Rename NavItem.module.css
    
    ---------
    
    Co-authored-by: Jane <jhur@us.ci.org>

---
+++ packages/react/src/components/Nav/NavItem.stories.tsx
@@ -0,0 +1,86 @@
+import { Meta } from '@storybook/react';
+import NavItem from './NavItem';
+
+const meta: Meta<typeof NavItem> = {
+  title: 'Components/Nav/NavItem',
+  component: NavItem,
+  argTypes: {
+    text: { control: 'text' },
+    href: { control: 'text' },
+    icon: { control: 'text' },
+    iconDescription: { control: 'text' },
+    target: {
+      control: { type: 'select' },
+      options: ['_blank', '_parent', '_self', '_top'],
+    },
+    isDisabled: {
+      control: { type: 'select' },
+      options: [true, false],
+    },
+    isActive: {
+      control: { type: 'select' },
+      options: [true, false],
+    },
+    isContained: {
+      control: { type: 'select' },
+      options: [true, false],
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ backgroundColor: '#fff', padding: '50px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+export const SingleNavItem = {
+  args: {
+    text: 'Hello World',
+    href: '#',
+    icon: 'house',
+    iconDescription: 'Website home',
+  },
+};
+
+export const NavItemWithSubnav = {
+  args: {
+    text: 'Hello World',
+    href: '#',
+    icon: 'house',
+    iconDescription: 'Website home',
+    subItems: [
+      {
+        text: 'Sub Nav Item 1',
+        href: '#',
+        icon: 'envelope',
+        iconDescription: 'Starred',
+      },
+      { text: 'Sub Nav Item 2', href: '#' },
+      { text: 'Sub Nav Item 3', href: '#' },
+    ],
+  },
+};
+
+export const ContainedNavItem = {
+  args: {
+    text: 'Hello World',
+    href: '#',
+    icon: 'house',
+    iconDescription: 'Website home',
+    isContained: true,
+    subItems: [
+      {
+        text: 'Sub Nav Item 1',
+        href: '#',
+        icon: 'envelope',
+        iconDescription: 'Starred',
+      },
+      { text: 'Sub Nav Item 2', href: '#' },
+      { text: 'Sub Nav Item 3', href: '#' },
+    ],
+  },
+};
---
+++ packages/react/src/components/Nav/NavItem.test.tsx
@@ -0,0 +1,43 @@
+import { render } from '@testing-library/react';
+import NavItem, { NavItemProps } from './NavItem';
+
+const testProps: NavItemProps = {
+  text: 'Hello World',
+  href: '#',
+  icon: 'house',
+  iconDescription: 'Website home',
+};
+
+const testSubItems: NavItemProps[] = [
+  { text: 'Sub Nav Item 1', href: '#' },
+  { text: 'Sub Nav Item 2', href: '#' },
+  { text: 'Sub Nav Item 3', href: '#' },
+];
+
+describe('NavItem', () => {
+  it('should render a single nav item with no subnav button or items', () => {
+    const { container } = render(<NavItem {...testProps} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render a nav item with subnav closed by default', () => {
+    const { container } = render(
+      <NavItem {...testProps} subItems={testSubItems} />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render a nav item with subnav open', () => {
+    const { container } = render(
+      <NavItem {...testProps} subItems={testSubItems} isSubNavInitiallyOpen />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render a contained navitem', () => {
+    const { container } = render(
+      <NavItem {...testProps} subItems={testSubItems} isContained />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
