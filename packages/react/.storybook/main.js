8ab65d4d30941f1d44f61b7fddffae5413f16a71
Kendrick Arnett <kmarnett@gmail.com>
2023-11-17 16:55:14 -0600

    Upgrade React Storybook to 7.5.

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,16 +1,13 @@
+import { dirname, join } from "path";
 export default {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-mdx-gfm")],
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
+    name: getAbsolutePath("@storybook/react-webpack5"),
+    options: {}
   },
   docs: {
-    autodocs: true,
+    autodocs: true
   },
   typescript: {
     check: false,
@@ -18,7 +15,7 @@ export default {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => {
+      propFilter: prop => {
         const commonForwardedPropsWeWantToDocument = /className|style/;
         if (commonForwardedPropsWeWantToDocument.test(prop.name)) {
           return true;
@@ -29,13 +26,19 @@ export default {
         if (prop.parent && /node_modules/.test(prop.parent.fileName)) {
           return false;
         }
-
         return true;
       },
       compilerOptions: {
         allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-    },
-  },
+        esModuleInterop: false
+      }
+    }
+  }
 };
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
