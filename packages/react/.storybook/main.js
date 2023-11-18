f2246ef4a8918448d52c0720c38f41a1fa3d3e4d
Kendrick Arnett <kmarnett@gmail.com>
2023-11-17 22:09:52 -0600

    Add Badge stories.

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,13 +1,18 @@
-import { dirname, join } from "path";
+import { dirname, join } from 'path';
 export default {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions"), getAbsolutePath("@storybook/addon-mdx-gfm")],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
+  ],
   framework: {
-    name: getAbsolutePath("@storybook/react-webpack5"),
-    options: {}
+    name: getAbsolutePath('@storybook/react-webpack5'),
+    options: {},
   },
   docs: {
-    autodocs: true
+    autodocs: false,
   },
   typescript: {
     check: false,
@@ -15,7 +20,7 @@ export default {
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: prop => {
+      propFilter: (prop) => {
         const commonForwardedPropsWeWantToDocument = /className|style/;
         if (commonForwardedPropsWeWantToDocument.test(prop.name)) {
           return true;
@@ -30,15 +35,15 @@ export default {
       },
       compilerOptions: {
         allowSyntheticDefaultImports: false,
-        esModuleInterop: false
-      }
-    }
-  }
+        esModuleInterop: false,
+      },
+    },
+  },
 };
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ */
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
--- packages/react/src/components/Badge/Badge.stories.tsx
+++ packages/react/src/components/Badge/Badge.stories.tsx
@@ -35,3 +35,38 @@ export const Default = {
     label: 'Badge',
   },
 };
+
+const Template = (args) => {
+  return <Badge {...args} />;
+};
+
+export const Status = (args) => (
+  <>
+    <Badge label="Primary" status="primary" {...args} />
+    <Badge label="Info" status="info" {...args} />
+    <Badge label="Success" status="success" {...args} />
+    <Badge label="Warning" status="warning" {...args} />
+    <Badge label="Danger" status="danger" {...args} />
+    <Badge label="Neutral" {...args} />
+  </>
+);
+
+export const BorderRadius = (args) => (
+  <>
+    <Badge label="Rounded Badge" {...args} />
+    <Badge label="Pill Badge" borderRadius="pill" {...args} />
+  </>
+);
+
+export const WithIcon = (args) => (
+  <>
+    <Badge
+      label="With iconPosition: end"
+      icon="ph-arrow-right"
+      iconPosition="end"
+      {...args}
+    />
+    <Badge label="With iconPosition: start" icon="ph-warning" {...args} />
+    <Badge label="" icon="ph-flag" {...args} />
+  </>
+);
