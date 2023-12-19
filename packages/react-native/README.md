81a77ced7f312974fc369966262e3ff7bd67bd95
Jane <jhur@us.ci.org>
2023-12-19 13:44:38 -0600

    Init React Native package

---
+++ packages/react-native/README.md
@@ -0,0 +1,31 @@
+# @compassion-design-system/react-native
+
+React Native components and patterns of the Compassion Design System.
+
+## Installation
+
+```sh
+npm install @compassion-design-system/react-native
+```
+
+## Usage
+
+```js
+import { multiply } from '@compassion-design-system/react-native';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ packages/react-native/example/src/App.tsx
@@ -0,0 +1,32 @@
+//@ts-nocheck
+import * as React from 'react';
+
+import { StyleSheet, View, Text } from 'react-native';
+import { multiply } from '@compassion-design-system/react-native';
+
+export default function App() {
+  const [result, setResult] = React.useState<number | undefined>();
+
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Results: {result}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
